34415b40a316bf0c3b79a601e78ba0c5481e42e4
JDGarciag12 <jdgarciag12uniandes.edu.co>
2025-01-26 18:40:09 -0500

    modificaciones individuales laboratorio 1

--- App/view.py
+++ App/view.py
@@ -57,6 +57,7 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
+    print("3- Cargar Tags de los libros")
     # TODO: Mods Lab 1, agregar la opcion 3.
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
@@ -135,7 +136,9 @@ def main():
         # Puede guiarse de las opciones 1 y 2.
         # Imprima el total de tags de los libros cargados.
         elif int(inputs[0]) == 3:
-            pass
+            print("Cargando información de tags de los libros....")
+            booksTags = load_books_tags(app)
+            print("Total de tags cargados: " + str(booksTags) + "\n")
 
         elif int(inputs[0]) == 0:
             working = False
